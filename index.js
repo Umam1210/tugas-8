499323f8d6152b62d023d8ff92028dc5125e4aea
khairul umam <khairulumam950@gmail.com>
2022-08-24 08:50:34 +0700

    update

--- index.js
+++ index.js
@@ -1,48 +1,66 @@
 const express = require('express')
 
 const app = express()
-port = 5000
+port = 3000
 
 app.set('view engine', 'hbs')
 app.use('/assets', express.static(__dirname + '/assets'))
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}))
+
+
 
 let isLogin = true
-let addProject = []
+let myProject = []
+
+app.get('/', function(request, response){
 
 
-app.get('/', function (request, response) {
-    response.render('index')
+    response.render('index',{myProject})
+
+    // console.log(myProject);
 })
-app.get('/form', function (request, response) {
+
+app.get('/form', function(request, response){
     response.render('form')
 })
-app.get('/project', function (request, response) {
-    response.render('project', { isLogin, addProject })
-})
-app.get('/project', function (request, response) {
-    response.render('project')
-})
-app.get('/add-project', function (request, response) {
-    response.render('add-project')
-})
-app.get('/desk-project/:index', function (request, response) {
+
+app.get('/detail-project/:index', function (request, response) {
     let index = request.params.index
-    let desk = {
-        name : addProject[index].name,
-        message : addProject[index].message,
-        startCalender : addProject[index].startCalender,
-        endCalender : addProject[index].endCalender,
-        duration : addProject[index].duration,
-        check1 : addProject[index].check1,
-        check2 : addProject[index].check2,
-        check3 : addProject[index].check3,
-        check4 : addProject[index].check4
+    let project = {
+        name : myProject[index].name,
+        message : myProject[index].message,
+        startDate : myProject[index].startDate,
+        endDate : myProject[index].endDate,
+        duration : myProject[index].duration,
+        check1 : myProject[index].check1,
+        check2 : myProject[index].check2,
+        check3 : myProject[index].check3,
+        check4 : myProject[index].check4
+
     }
-    response.render('desk-project', {index, desk})
+    response.render('detail-project', { index, project})
 })
 
-app.post('/add-project', function (request, response) {
+app.get('/delete-project/:index', function (request, response) {
+    let index = request.params.index
+    // console.log(index);
+
+    myProject.splice(index, 1)
+    // console.log(addProject);
+
+    response.redirect('/')
+})
+
+
+app.get('/add-project', function(request, response){
+    response.render('add-project')
+})
+
+
+
+
+app.post('/add-project', function(request, response){
+    // console.log(request.body);
     let name = request.body.inputName
     let startDate = request.body.inputStartDate
     let endDate = request.body.inputEndDate
@@ -52,147 +70,118 @@ app.post('/add-project', function (request, response) {
     let check2 = request.body.check2
     let check3 = request.body.check3
     let check4 = request.body.check4
-
-
-    // console.log(check1);
-    // console.log(check2);
-    // console.log(check3);
-    // console.log(check4);
-
-    monthIndex = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
-
-    startDate = new Date(startDate)
-    let date = startDate.getDate()
-    let month = startDate.getMonth()
-    month = monthIndex[month]
-    let year = startDate.getFullYear()
-
-    endDate = new Date(endDate)
-    let endDatePost = endDate.getDate()
-    let endMonth = endDate.getMonth()
-    endMonth = monthIndex[endMonth]
-    let endYear = endDate.getFullYear()
-
-    let duration = endDate - startDate
-    let milisecond = 1000
-    let secondInHours = 3600
-    let hoursInDay = 24
-    let dayInMonth = 30
-
-    duration = Math.floor(duration / (milisecond * secondInHours * hoursInDay))
-    dayInMonth = Math.floor(duration / (dayInMonth))
-
-    let startCalender = `${date} ${month} ${year}`
-    let endCalender = `${endDatePost} ${endMonth} ${endYear}`
-
-    let dataProject = {
+    
+    let project = {
         name,
-        startDate,
-        endDate,
-        endMonth,
-        month,
-        endYear,
-        year,
-        date,
-        endDatePost,
-        startCalender,
-        endCalender,
+        startDate : getFullTime(new Date (startDate)),
+        endDate: getFullTime(new Date (endDate)),
+        duration : getDistanceTime(new Date(startDate), new Date(endDate)),
         message,
         image,
-        dayInMonth,
-        duration,
         check1,
         check2,
         check3,
         check4
     }
-    addProject.push(dataProject)
-
-
-    response.redirect('/project')
+    
+    myProject.push(project)
+    response.redirect('/')
 })
+
+
 
 app.get('/edit-project/:index', function (request, response) {
     let index = request.params.index
-
     let project = {
-         name : addProject[index].name,
-         startCalender : addProject[index].startCalender,
-         endCalender : addProject[index].endCalender,
-         message : addProject[index].message
+        name : myProject[index].name,
+        message : myProject[index].message,
+        startDate : myProject[index].startDate,
+        endDate : myProject[index].endDate,
+        duration : myProject[index].duration,
+        check1 : myProject[index].check1,
+        check2 : myProject[index].check2,
+        check3 : myProject[index].check3,
+        check4 : myProject[index].check4
+
     }
-    response.render('edit-project', { index, project})
+    response.render('edit-project', {index, project})
 })
 
-app.post('/edit-project/:index', function (request, response) {
-    let index = request.params.index
-    
-    addProject[index].name = request.body.inputName
-    addProject[index].message = request.body.exampleFormControlTextarea1
 
+app.post('/edit-project/:index', function(request, response){
+    let index = request.params.index
+    let name = request.body.inputName
+    let startDate = request.body.inputStartDate
+    let endDate = request.body.inputEndDate
+    let message = request.body.exampleFormControlTextarea1
+    let image = request.body.inputFile
     let check1 = request.body.check1
     let check2 = request.body.check2
     let check3 = request.body.check3
     let check4 = request.body.check4
-
-
-
-    addProject[index].check1 = request.body.check1
-    addProject[index].check2 = request.body.check2
-    addProject[index].check3 = request.body.check3
-    addProject[index].check4 = request.body.check4
     
+    let project = {
+        name,
+        startDate : getFullTime(new Date (startDate)),
+        endDate: getFullTime(new Date (endDate)),
+        duration : getDistanceTime(new Date(startDate), new Date(endDate)),
+        message,
+        image,
+        check1,
+        check2,
+        check3,
+        check4
+    }
+
+    myProject[index]=project
 
 
-    let startDate = request.body.inputStartDate
-    let endDate = request.body.inputEndDate
-   
-    monthIndex = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
 
-    startDate = new Date(startDate)
-    let date = startDate.getDate()
-    let month = startDate.getMonth()
-    month = monthIndex[month]
-    let year = startDate.getFullYear()
-
-
-    endDate = new Date(endDate)
-    let endDatePost = endDate.getDate()
-    let endMonth = endDate.getMonth()
-    endMonth = monthIndex[endMonth]
-    let endYear = endDate.getFullYear()
-
-    let duration = endDate - startDate
-    let milisecond = 1000
-    let secondInHours = 3600
-    let hoursInDay = 24
-    let dayInMonth = 30
-
-    duration = Math.floor(duration / (milisecond * secondInHours * hoursInDay))
-    dayInMonth = Math.floor(duration / (dayInMonth))
-
-    let startCalender = `${date} ${month} ${year}`
-    let endCalender = `${endDatePost} ${endMonth} ${endYear}`
-    
-    addProject[index].duration = duration
-    addProject[index].startCalender = startCalender
-    addProject[index].endCalender = endCalender
-
-
-    response.redirect('/project')
-}
-)
-
-
-app.get('/delete-project/:index', function (request, response) {
-    let index = request.params.index
-    // console.log(index);
-
-    addProject.splice(index, 1)
-    // console.log(addProject);
-
-    response.redirect('/project')
+    response.redirect('/')
 })
-app.listen(port, function () {
+
+
+
+function getFullTime(time){
+
+    let month = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
+  
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+  
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+    
+    let fullTime = `${date} ${month[monthIndex]} ${year}`
+    return fullTime
+  }
+
+  function getDistanceTime(start, end){
+
+    let startDate = new Date(start)
+    let endDate = new Date(end)
+  
+    let distance = endDate - startDate
+    // console.log(distance);
+  
+    let milisecond = 1000 
+    let secondInHours = 3600 
+    let hoursInDay = 24 
+  
+    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
+    let distanceHours = Math.floor(distance / (milisecond * 60 * 60))
+    let distanceMinutes = Math.floor(distance / (milisecond * 60))
+    let distanceSeconds = Math.floor(distance / milisecond)
+    
+    return `${distanceDay} day`
+    
+  }
+
+
+
+
+
+app.listen(port, function(){
     console.log(`server running on port ${port}`);
 })
